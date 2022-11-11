4196f32507a572a897659d40eab3f7f9528e6e3f
pjgabriel016 <117953479+pjgabriel016@users.noreply.github.com>
2022-11-11 08:19:49 +0800

    Update streamlit_app.py
    Adjust location of pick option

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ streamlit.text('🥑🍞 Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -17,5 +19,4 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
